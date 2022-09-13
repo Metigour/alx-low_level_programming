a9a7cf97aa5bbebec397943f40dabcc13aba7ae6
awodire oladayo <oladayo_e@yahoo.com>
2022-09-13 08:19:20 -0700

    cp3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include "main.holbert"
+#include "main.h"
 
 /**
- * main - print Holbert, followed by a new line
+ * main - print _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
