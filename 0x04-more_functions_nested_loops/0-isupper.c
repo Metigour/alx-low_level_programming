b83965a5b7e4f4ea473e4c6a2af1ab76ac9eeb4d
awodire oladayo <oladayo_e@yahoo.com>
2022-09-15 15:05:00 -0700

    cp5

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,17 +5,7 @@
  * @c: print int
  * Return: give 1 or 0
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1)
-	}
-
-	else
-	{
-		return (0);
-	}
-
+	return (c >= 'A' && c <= 'Z');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _isupper(int c);
 int _isdigit(int c);
