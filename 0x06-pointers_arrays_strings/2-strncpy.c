a31782e094531fccaaa50aa948f13c2a571a5496
awodire oladayo <oladayo_e@yahoo.com>
2022-09-21 13:14:57 -0700

    pas1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,30 @@
 #include "main.h"
+
 /**
- * _strncpy - copie a string
- * @dest:char
- * @src:char
- * @n:int
- * Return:char
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	i = 0;
-	while (i < n && *(src + i))
+
+	while (src[i] != '\0' && i < n)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 		i++;
 	}
+
 	while (i < n)
 	{
-		*(dest + i) = '\0';
+		dest[i] = '\0';
 		i++;
 	}
+
 	return (dest);
 }
