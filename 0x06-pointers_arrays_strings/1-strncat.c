80178dd5737df830bc7d7e088405514f3b956e14
awodire oladayo <oladayo_e@yahoo.com>
2022-09-22 13:45:50 -0700

    pas1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,24 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates n bytes from a string to another
- * @dest: destination string
- * @src: source string
- * @n: number of bytes of str to concatenate
- *
- * Return: a pointer to the resulting string dest
+ * _strncat - concatenates two strings
+ * @dest:char dest
+ * @src: char src
+ * @n:length int
+ * Return:char
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+int i, j;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	
-	dest[i] = '\0';
-
-	return (dest);
+for (i = 0; dest[i] != '\0'; i++)
+{
+continue;
+}
+for (j = 0; src[j] != '\0' && j < n; j++)
+{
+dest[i + j] = src[j];
+}
+dest[i + j] = '\0';
+return (dest);
 }
