f052c33bb765ab8219925cd9c839aa870aa98638
awodire oladayo <oladayo_e@yahoo.com>
2022-09-26 20:49:40 -0700

    pas7

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,4 +4,28 @@
  * @s:string
  * @accept:string
  * Return:unsigned int
-9999999999999999999
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int x;
+int i;
+
+x = 0;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+x++;
+break;
+}
+else if (accept[i + 1] == '\0')
+{
+return (x);
+}
+}
+s++;
+}
+return (x);
+}
